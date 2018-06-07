37a42a138546b6f8024a2fad03d354a98bbcdb4a
Eduardo Model <edu_model@hotmail.com>
2018-06-07 20:24:11 -0300

    Adicionado a rotina PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
-let express = require('express')
-let bodyParser = require('body-parser')
+const _ = require('lodash')
 
-let {ObjectID} = require('mongodb')
+const express = require('express')
+const bodyParser = require('body-parser')
+const {ObjectID} = require('mongodb')
 
 
 let {mongoose} = require('./db/mongoose.js')
@@ -67,7 +68,36 @@ app.delete('/todos/:id', (req, res) => {
 	}).catch((e) => res.status(400).send())
 })
 
+app.patch('/todos/:id', (req, res) => {
+	let id = req.params.id
+	//o metodo pick retira propriedades de um objeto, passando-as atraves do argumento da função
+	let body = _.pick(req.body, ['text', 'completed'])
 
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send()
+	}
+
+	if(_.isBoolean(body.completed) && body.completed){
+		body.completedAt = new Date().getTime()
+	}else{
+		body.completed = false
+		body.completedAt = null
+	}
+
+	Todo.findByIdAndUpdate(id, {
+		$set: body
+	}, {
+		new: true
+	}).then((todo) => {
+		if(!todo){
+			return res.status(404).send()
+		}
+
+		res.send({todo})
+	}).catch((e) => res.status(400).send())
+
+
+})
 
 app.listen(port, () => {
 	console.log(`Iniciado na porta ${port}`)
